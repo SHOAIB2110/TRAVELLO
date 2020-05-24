09c3ff9c4050ca7f4b25d460098dc689f91f9627
unknown <shoaibsayyad2110@gmail.com>
2020-05-24 10:45:41 -0700

    fourth cot

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const helmet = require('helmet')
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static('public'))
+app.use(express.static('files'))
 
 app.use(helmet.noSniff())
 
